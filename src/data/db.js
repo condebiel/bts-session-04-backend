5ae10c0d290973514c714f450bbc0ced6e70ddec
condebiel <bielbcm@gmail.com>
2023-05-31 09:29:35 +0200

    First commit

---
+++ src/data/db.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    "id": 1,
+    "name": "iPhone 13",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Apple",
+    "price": 999,
+    "category": "Electronics"
+  },
+  {
+    "id": 2,
+    "name": "Samsung Galaxy S21",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Samsung",
+    "price": 899,
+    "category": "Electronics"
+  },
+  {
+    "id": 3,
+    "name": "Sony PlayStation 5",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Sony",
+    "price": 499,
+    "category": "Gaming"
+  },
+  {
+    "id": 4,
+    "name": "Canon EOS R5",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Canon",
+    "price": 3899,
+    "category": "Electronics"
+  },
+  {
+    "id": 5,
+    "name": "Nike Air Max 270",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Nike",
+    "price": 150,
+    "category": "Fashion"
+  },
+  {
+    "id": 6,
+    "name": "Apple MacBook Pro",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Apple",
+    "price": 1999,
+    "category": "Electronics"
+  },
+  {
+    "id": 7,
+    "name": "Fitbit Versa 3",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Fitbit",
+    "price": 229,
+    "category": "Electronics"
+  },
+  {
+    "id": 8,
+    "name": "Amazon Echo Dot",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Amazon",
+    "price": 49,
+    "category": "Electronics"
+  },
+  {
+    "id": 9,
+    "name": "Dyson V11",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida nibh justo, at congue felis commodo scelerisque. Nulla at est vitae ex ultricies pharetra. Sed id tincidunt ligula.",
+    "brand": "Dyson",
+    "price": 599,
+    "category": "Home Appliances"
+  },
+]
---
+++ src/routes/index.js
@@ -0,0 +1,43 @@
+import { Router } from 'express'
+import products from '../data/db.js'
+
+const router = Router()
+
+// root
+router.get('/', (request, response) => {
+	// key value object (json object)
+	let person = {
+		name: 'Biel v2',
+		age: 30,
+	}
+
+	response.json(person)
+})
+
+router.get('/products', (request, response) => {
+	response.json(products)
+})
+
+router.get('/products/:category', (request, response) => {
+	const { category } = request.params
+
+	const filteredProducts = products.filter(function(product) {
+		const productCategory = product.category.toLowerCase()
+		const paramCategory = category.toLowerCase()
+		return productCategory === paramCategory
+	})
+
+	response.json(filteredProducts)
+})
+
+router.get('/product/:id', (request, response) => {
+	const { id } = request.params
+
+	const filteredProducts = products.filter(function(product) {
+		return product.id === Number(id)
+	})
+
+	response.json(filteredProducts.pop())
+})
+
+export default router
